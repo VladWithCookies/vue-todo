e0263ae362d8be2104d74a4c10cd1ada8af05689
Vlad <summervibeswith9pm@gmail.com>
2018-02-23 21:15:13 +0200

    Create project controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,29 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: %i(update destroy)
+
   def index
+    projects = Project.all
+    render json: projects
+  end
+
+  def create
+    project = Project.create(title: params[:title])
+    render json: project
+  end
+
+  def update
+    @project.update(title: params[:title])
+    render json: @project
+  end
+
+  def destroy
+    @project.destroy
+    render json: @project
+  end
+
+  private
+
+  def set_project
+    @project ||= Project.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root to: 'projects#index'
+
+  resources :projects, only: %i(index create update destroy)
 end
